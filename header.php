b72fa5d3e13a23e8ffb18e441ca9cac4a517dd3c
gribeiroq <guilherme@sistemaaula.com.br>
2021-07-28 14:18:07 -0300

    Versão Final Teste Empresta

--- header.php
+++ header.php
@@ -10,96 +10,95 @@
 	  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
     <link href="style.css" rel="stylesheet">
     <!--Font Awesome-->
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     
     <title>Empresta Bem Melhor</title>
   </head>
   <body>
         <!--Menu botões-->
-        <div class="bg-bs-white border border-dark mobile-style">
-        <div class="container">
-            <div class="row">
-            <div class="col-6"></div>
-            <div class="col-6">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item active">
-                                    <button type="button" class="button-sup btn btn-outline-emp-orange btn-sm" href="#">Seja um Franqueado <span class="sr-only">(current)</span></button>
-                                </li>
-                                <li class="link-sup nav-item">
-                                    <a class="nav-link" href="#">Blog Empresta</a>
-                                </li>
-                                <li class="link-sup nav-item">
-                                    <a class="nav-link" href="#">Trabalhe Conosco</a>
-                                </li>
-                                <li class="lead icon">
-                                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                                </li>
-                                <li class="lead icon">
-                                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                                </li>
-                                <li class="lead icon">
-                                    <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-                                </li>
-                                <li class="lead icon">
-                                    <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i>
-                                </li>
-                            </ul>
-                        </div>
-                </nav>
-            </div>         
+    <div class="bg-bs-white border-bottom bg-white fixed-top pb-3">
+      <div class="container mobile-style">
+        <div class="row">
+          <div class="col-6"></div>
+          <div class="col-6">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <button type="button" class="button-sup btn btn-outline-emp-orange btn-sm" href="#">Seja um Franqueado <span class="sr-only">(current)</span></button>
+                  </li>
+                  <li class="link-sup nav-item">
+                    <a class="nav-link" href="#">Blog Empresta</a>
+                  </li>
+                  <li class="link-sup nav-item">
+                    <a class="nav-link" href="#">Trabalhe Conosco</a>
+                  </li>
+                  <li class="lead icon">
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  </li>
+                  <li class="lead icon">
+                    <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                  </li>
+                  <li class="lead icon">
+                    <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                  </li>
+                  <li class="lead icon">
+                    <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>         
         </div>
-    </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col"></div>
+            <div>
+              <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                <a class="navbar-brand col-6" href="#"><img class="img-fluid menu-img" src="https://empresta.com.br/wp-content/themes/empresta/assets/images/logo.svg" alt="logotipo empresta"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse main-menu" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Empréstimos
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Dropdown 01</a>
+                        <a class="dropdown-item" href="#">Dropdown 02</a>
+                        <a class="dropdown-item" href="#">Dropdown 03</a>
+                      </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Cartão
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Dropdown 01</a>
+                        <a class="dropdown-item" href="#">Dropdown 02</a>
+                        <a class="dropdown-item" href="#">Dropdown 03</a>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Franquias</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">A Empresta</a>
+                    </li>
+                  </ul>
                 </div>
-                    <!-- </div> -->
-                <div>
-                  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-                    <a class="navbar-brand col-6" href="#"><img class="img-fluid menu-img" src="assets/logo.png" alt="logotipo empresta"></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse main-menu" id="navbarNavDropdown">
-                      <ul class="navbar-nav">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Empréstimos
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Dropdown 01</a>
-                            <a class="dropdown-item" href="#">Dropdown 02</a>
-                            <a class="dropdown-item" href="#">Dropdown 03</a>
-                          </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Cartão
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Dropdown 01</a>
-                            <a class="dropdown-item" href="#">Dropdown 02</a>
-                            <a class="dropdown-item" href="#">Dropdown 03</a>
-                          </div>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Franquias</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">A Empresta</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </nav>
-                </div>
+              </nav>
             </div>
+          </div>
         </div>
+      </div>
     </div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+    <!--Pesquisar-->
     <div class="container">
       <div class="row">
         <div class="col-sm search-sup">
@@ -15,212 +16,134 @@
                 </div>
               </div>
             </div>
-              <a class="nav-link" href="#">Clique aqui para ver todas as lojas.</a>
           </div>
+          <a class="nav-link" href="#">Clique aqui para ver todas as lojas.</a>
         </div>
         <div class="col-sm">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.141421962995!2d-43.94005058446856!3d-19.918444443095197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699fb6dcb2f0d%3A0x7131eec9567c2dc7!2sEmpresta%20-%20Bem%20Melhor!5e0!3m2!1spt-BR!2sbr!4v1627321961867!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
       </div>
     </div>
-<!--Melhor Resultado-->
-    <div class="container">
+
+    <!--Melhor Resultado-->
+    <div class="container pt-3">
+      <h3 class="resultado-titulo">Melhor Resultado:</h3>
       <div class="row">
-        <div class="col-6 resultado">
-          <h3 class="resultado-titulo">Melhor Resultado:</h3>
-          <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
+        <div class="col-sm">
+          <strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)
           <h2 class="nota"><strong>Empresta Belo Horizonte</strong></h2>
-        <div class="col-12 recuo">
-          <div class="container endereco">
-            <div class="row">
-              <div class="col-1">
-                <i class="nota fa fa-map-marker"></i>
-              </div>
-              <div class="col-11">
-                <strong>Av. Afonso Pena 1005. LJ. 02 - Centro</strong>
+            <div> 
+              <i class="nota fa fa-map-marker"></i>
+              <strong>Av. Afonso Pena 1005. LJ. 02 - Centro</strong>
+            </div>
+            <div>
+              <i class="nota fa fa-phone"></i>
+              <strong>(31) 3239-1005</strong>
+            </div>
+            <div class="pt-3 pl-5">
+              <i class="nota fa fa-clock-o"></i>
+              <strong>Horários</strong>
+            </div>
+            <div>
+              <table class="table table-borderless">
+                <tbody>
+                  <tr>
+                    <td>Domingo</td>
+                    <td>Fechado</td>
+                  </tr>
+                  <tr>
+                    <td>Segunda-Feira</td>
+                    <td>08h00 - 18h00</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Terça-Feira</th>
+                    <th scope="row">08h00 - 18h00</th>
+                  </tr>
+                  <tr>
+                    <td>Quarta-Feira</td>
+                    <td>08h00 - 18h00</td>
+                  </tr>
+                  <tr>
+                    <td>Quinta-Feira</td>
+                    <td>08h00 - 18h00</td>
+                  </tr>
+                  <tr>
+                    <td>Sexta-Feira</td>
+                    <td>08h00 - 18h00</td>
+                  </tr>
+                  <tr>
+                    <td>Sábado</td>
+                    <td>Fechado</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class='pb-3'>
+              <div class="row">
+                <div class="col-sm pb-3">
+                  <button type="button" class="btn btn-emp-orange btn-sm btn-block">Me ligue</button>
+                </div>
+                <div class="col-sm" style="text-align: center">
+                  <button type="button" class="btn btn-emp-zap btn-sm btn-block">Falar pelo Whatsapp</button>
+                </div>
               </div>
             </div>
           </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-1">
-                <i class="nota fa fa-phone"></i>
-              </div>
-              <div class="col-11">
-                <strong>(31) 3239-1005</strong>
-              </div>
-            </div>
-          </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-1 horario">
-                <i class="fa fa-clock-o"></i>
-              </div>
-              <div class="col-11">
-            <table class="table table-borderless">
-            <tbody>
-              <tr>
-                <td>Domingo</td>
-                <td>Fechado</td>
-              </tr>
-              <tr>
-                <td>Segunda-Feira</td>
-                <td>08h00 - 18h00</td>
-              </tr>
-              <tr>
-                <th scope="row">Terça-Feira</th>
-                <th scope="row">08h00 - 18h00</th>
-              </tr>
-              <tr>
-                <td>Quarta-Feira</td>
-                <td>08h00 - 18h00</td>
-              </tr>
-              <tr>
-                <td>Quinta-Feira</td>
-                <td>08h00 - 18h00</td>
-              </tr>
-              <tr>
-                <td>Sexta-Feira</td>
-                <td>08h00 - 18h00</td>
-              </tr>
-              <tr>
-                <td>Sábado</td>
-                <td>Fechado</td>
-              </tr>
-            </tbody>
-            </table></div>
-          <div class="container">
-            <div class="row">
-              <div class="col-6">
-                <button type="button" class="btn btn-emp-orange btn-sm btn-block">Me ligue</button>
-              </div>
-              <div class="col-6">
-                <button type="button" class="btn btn-emp-zap btn-sm btn-block">Falar pelo Whatsapp</button>
-              </div>
-            </div>
-          </div>
+        <div class="col-sm">
+          <img class='img-fluid' src="assets/loja-principal.png" alt="Empresta Belo Horizonte">
         </div>
       </div>
     </div>
-      </div>
-        <div class="col-6 resultado-img">
-          <img class="img-fluid" src="assets/loja-principal.png" alt="Empresta Belo Horizonte">
+
+    <!--Outros Resultados-->
+    <div class="bg-emp-blue pb-3">
+      <div class="container resultado-out">
+        <div class="row">
+          <h2>Outras Lojas</h2>
+          <h2><strong>4 Resultados</strong> em Belo Horizonte:</h2>
         </div>
+        <ul class="list-group">
+          <li class="d-flex rounded pb-1">
+            <img src="https://empresta.com.br/wp-content/uploads/2021/07/frente-loja.png" alt="Empresta Belo Horizonte" class="resultado-out-img ">
+            <div class='pl-2 resultado-out-campo'>
+              <h5 class="nota"><strong>Empresta Bem Melhor</strong></h5>
+              <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
+              <span style='color:black'>Av. Afonso Pena 1005. LJ. 02 - Centro
+              Aberto até as <strong>18:00 · </strong> (31) 3239-1005</span>
+            </div>
+          </li>
+         <li class="d-flex rounded pb-1">
+            <img src="https://empresta.com.br/wp-content/uploads/2021/07/frente-loja.png" alt="Empresta Belo Horizonte" class="resultado-out-img ">
+            <div class='pl-2 resultado-out-campo'>
+              <h5 class="nota"><strong>Empresta Bem Melhor</strong></h5>
+              <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
+              <span style='color:black'>Av. Afonso Pena 1005. LJ. 02 - Centro
+              Aberto até as <strong>18:00 · </strong> (31) 3239-1005</span>
+            </div>
+          </li>
+          <li class="d-flex rounded pb-1">
+            <img src="https://empresta.com.br/wp-content/uploads/2021/07/frente-loja.png" alt="Empresta Belo Horizonte" class="resultado-out-img ">
+            <div class='pl-2 resultado-out-campo'>
+              <h5 class="nota"><strong>Empresta Bem Melhor</strong></h5>
+              <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
+              <span style='color:black'>Av. Afonso Pena 1005. LJ. 02 - Centro
+              Aberto até as <strong>18:00 · </strong> (31) 3239-1005</span>
+            </div>
+          </li>
+          <li class="d-flex rounded pb-1">
+            <img src="https://empresta.com.br/wp-content/uploads/2021/07/frente-loja.png" alt="Empresta Belo Horizonte" class="resultado-out-img ">
+            <div class='pl-2 resultado-out-campo'>
+              <h5 class="nota"><strong>Empresta Bem Melhor</strong></h5>
+              <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
+              <span style='color:black'>Av. Afonso Pena 1005. LJ. 02 - Centro
+              Aberto até as <strong>18:00 · </strong> (31) 3239-1005</span>
+            </div>
+          </li>
+        </ul>
       </div>
-    </div>
-<!--Outros Resultados-->
-    <div class="bg-emp-blue">
-        <div class="container resultado-out">
-          <div class="row">
-            <div class="col-8">
-              <h2>Outras Lojas</h2>
-              <h2><strong>4 Resultados</strong> em Belo Horizonte:</h2>
-            </div>
-            <div class="col-8">
-              <div class="container">
-                <div class="row">
-              <div class="col-3">
-                <img src="assets/thumb.png" alt="Empresta Belo Horizonte" class="resultado-out-img">
-              </div>
-              <div class="col-9 resultado-out-campo">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-9">
-                    <h4 class="nota"><strong>Empresta Belo Horizonte</strong></h4>
-                    <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
-                    <div>Av. Afonso Pena 1005. LJ. 02 - Centro</div>
-                    <div>Aberto até as <strong>18:00 · </strong> (31) 3239-1005</div>
-                    </div>
-                    <div class="col-3">
-                    <button type="button" class="btn btn-emp-orange btn-sm resultado-button pull-right">Me ligue</button>
-                    </div>
-                  </div>
-                </div>
-              
-              </div>
-              </div>
-              </div>
-            </div>
-            <div class="col-8">
-              <div class="container">
-                <div class="row">
-              <div class="col-3">
-                <img src="assets/thumb.png" alt="Empresta Belo Horizonte" class="resultado-out-img">
-              </div>
-              <div class="col-9 resultado-out-campo">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-9">
-                    <h4 class="nota"><strong>Empresta Belo Horizonte</strong></h4>
-                    <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
-                    <div>Av. Afonso Pena 1005. LJ. 02 - Centro</div>
-                    <div>Aberto até as <strong>18:00 · </strong> (31) 3239-1005</div>
-                    </div>
-                    <div class="col-3">
-                    <button type="button" class="btn btn-emp-orange btn-sm resultado-button pull-right">Me ligue</button>
-                    </div>
-                  </div>
-                </div>
-              
-              </div>
-              </div>
-              </div>
-            </div>
-            <div class="col-8">
-              <div class="container">
-                <div class="row">
-              <div class="col-3">
-                <img src="assets/thumb.png" alt="Empresta Belo Horizonte" class="resultado-out-img">
-              </div>
-              <div class="col-9 resultado-out-campo">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-9">
-                    <h4 class="nota"><strong>Empresta Belo Horizonte</strong></h4>
-                    <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
-                    <div>Av. Afonso Pena 1005. LJ. 02 - Centro</div>
-                    <div>Aberto até as <strong>18:00 · </strong> (31) 3239-1005</div>
-                    </div>
-                    <div class="col-3">
-                    <button type="button" class="btn btn-emp-orange btn-sm resultado-button pull-right">Me ligue</button>
-                    </div>
-                  </div>
-                </div>
-              
-              </div>
-              </div>
-              </div>
-            </div>
-            <div class="col-8">
-              <div class="container">
-                <div class="row">
-              <div class="col-3">
-                <img src="assets/thumb.png" alt="Empresta Belo Horizonte" class="resultado-out-img">
-              </div>
-              <div class="col-9 resultado-out-campo">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-9">
-                    <h4 class="nota"><strong>Empresta Belo Horizonte</strong></h4>
-                    <div class="nota"><strong>4,5</strong><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>(327)</div>
-                    <div>Av. Afonso Pena 1005. LJ. 02 - Centro</div>
-                    <div>Aberto até as <strong>18:00 · </strong> (31) 3239-1005</div>
-                    </div>
-                    <div class="col-3">
-                    <button type="button" class="btn btn-emp-orange btn-sm resultado-button pull-right">Me ligue</button>
-                    </div>
-                  </div>
-                </div>
-              
-              </div>
-              </div>
-              </div>
-            </div>
-            <div class="pull-right">
-              <a class="nota" href="#"><strong>Mais Lojas</strong></a>
-          </div>
-          </div>
-        </div>
+      <div class="text-center">
+        <a class="nota" href="#"><strong>Mais Lojas</strong></a>
+      </div>
     </div>
     
 <?php get_footer(); ?>
