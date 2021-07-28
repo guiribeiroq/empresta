c2aeb70d32463b8043a4be7d50a67ad0a2d222b1
gribeiroq <guilherme@sistemaaula.com.br>
2021-07-28 08:44:09 -0300

    Correção css Wordpress

--- header.php
+++ header.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="main.css" rel="stylesheet">
-    <link href="empresta.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <link href="style.css" rel="stylesheet">
     <!--Font Awesome-->
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     
